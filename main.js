bf22ef22f31c65c1855ad11005717529d4d82887
Jing <jingkcheng@hotmail.com>
2019-07-15 19:21:02 -0400

    this, template literals, destructuring objects/arrays, object literals, for of loop, spread, rest, includes

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const/var funcName parameter => { statements }
 const/var funcName parameter => expression
 */
 const addNums = (num1 = 1, num2 = 1) => num1 + num2;
+console.log("Arrow Functions");
 console.log(addNums(5, 5));
 
 //High Order Array Methods - forEach, map, filter
@@ -30,6 +31,7 @@ let todos = [
 
 //forEach
 todos.forEach((todo) => {
+    console.log("forEach")
     console.log(todo.id)
 });
 
@@ -37,6 +39,7 @@ todos.forEach((todo) => {
 const todoText = todos.map((todo) => {
     return todo.text;
 });
+console.log("map")
 console.log(todoText);
 
 //filter
@@ -46,4 +49,84 @@ const todoCompleted = todos.filter((todo) => {
     return todo.text;
 });
 //Chaining high order arrays to get specific information
+console.log("filter and chaining");
 console.log(todoCompleted);
+
+//this keyword
+//method -> obj
+//function -> global (window, global)
+function Video(title){
+    this.title = title;
+    console.log(this);
+}
+
+const v = new Video('b'); 
+
+//Template Literals
+let word1 = "Jing";
+let word2 = "Cheng";
+
+const fullName = `${word1} ${word2}`;
+console.log("Template Literals");
+console.log(fullName);
+
+//Destructuring Objects
+const personalInfo = {
+    firstName: 'Jing',
+    lastName: 'Cheng',
+    city: 'Mississauga'
+};
+
+const {firstName: fn, lastName: ln} = personalInfo; //Destruct personalInfo Object
+console.log("Destructuring Objects");
+console.log(`${fn} ${ln}`);
+
+//Destructuring Arrays
+let [firstName, middleName, lastName] = ['Jing', 'Coding Noob', 'Cheng'];
+lastName = 'Yun';
+console.log("Destructuring Arrays");
+console.log(lastName);
+
+//Object Literal
+function addressMaker(city, state){
+    const newAddress = {city, state};
+    console.log("Object Literals");
+    console.log(newAddress);
+}
+
+addressMaker('Mississauga', 'Ontario');
+
+//For of loop - Allows us to iterate through an array or string
+let herName = "Jenny Yun";
+console.log("For of loop");
+for(const char of herName){
+    console.log(char);
+}
+
+//Spread Operator - Unwraps whatever it references
+let example = [1, 2, 3, 4, 5];
+let example2 = [...example]; 
+console.log("Spread Operator");
+console.log(example2);
+
+//Rest Operator 
+function add(...nums){
+    console.log("Rest Operator");
+    console.log(nums);
+}
+add(4, 5, 6, 7, 8);
+
+//Includes() - Returns a boolean value of true/false if the array contains the value
+let numArray = [1, 2, 3, 4, 5];
+console.log('Includes()');
+console.log(numArray.includes(0));
+
+//Let vs Const
+//Let - Stricter version of var that uses block scope
+//Const - Constant that cannot be changed (read only)
+
+//Import & Export
+// export const data = [1, 2, 3];
+// import{ data } from './main.js';
+// console.log(data);
+
